4a5ed26109575d32c2608efdf09aa41f28c2257a
Muthamizh23 <muthamizh1023@gmail.com>
2024-08-09 09:51:00 +0530

    database url

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -1,12 +1,22 @@
-import mongoose from "mongoose";
-
 export const dbConnection = () => {
-  mongoose
-    .connect("mongodb://localhost:27017/thamizh")
-    .then(() => {
-      console.log("Connected to database!");
-    })
-    .catch((err) => {
-      console.log(`Some error occured while connecting to database: ${err}`);
-    });
-};
+  mongoose.connect('mongodb+srv://Muthamizhhari:Muthamizhhari@thamizh.xxoiz.mongodb.net/thamizh?retryWrites=true&w=majority&appName=thamizh')
+      .then( () => {
+          console.log('Connected to database ')
+      })
+      .catch( (err) => {
+          console.error(`Error connecting to the database. \n${err}`);
+      })
+  
+      
+    // mongoose
+    //   .connect('mongodb+srv://Muthamizhhar:Muthamizhhari@thamizh.xxoiz.mongodb.net/?retryWrites=true&w=majority&appName=thamizh', {
+    //     dbName: "thamizh",
+    //   })
+    //   .then(() => {
+    //     console.log("Connected to database!");
+    //   })
+    //   .catch((err) => {
+    //     console.log(err);
+    //     console.log(Some error occured while connecting to database: ${err});
+    //   });
+  };
