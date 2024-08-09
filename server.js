428515a94ba5a7967aa3c7f10b9f33bd592e05d2
Muthamizh23 <muthamizh1023@gmail.com>
2024-08-09 10:41:41 +0530

    database url

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 import app from "./app.js";
+import cloudinary from "cloudinary";
 
+cloudinary.v2.config({
+  cloud_name: "dixa1paoe",
+  api_key: "972445983997627",
+  api_secret: "ntrNHR2sjgjrZaJL3aD6aXlaUEc",
+});
 
 app.listen(4000, () => {
   console.log(`Server running on port 4000`);
