df05b29ca7439b2c8df5f15c60a31797ff567b98
kavinkumar M <muthamizh1023@gmail.com>
2024-08-13 22:06:37 +0530

    auth fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,6 +5,7 @@ export const sendToken = (user, statusCode, message, res) => {
       Date.now() + 1 * 24 * 3600000
     ),
     httpOnly: true,
+    maxAge: 24 * 60 * 60,
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
