7c61d0f026bfaa6e07c1ce467e2b60ce2b779e85
kavinkumar M <muthamizh1023@gmail.com>
2024-08-13 23:26:04 +0530

    auth fix

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -5,10 +5,11 @@ export const sendToken = (user, statusCode, message, res) => {
     //   Date.now() + 1 * 24 * 3600000
     // ),
     // maxAge: 24 * 60 * 60,
+    expires: token.expiresIn,
     httpOnly: true ,
     secure: true,
     sameSite: 'none',
-    domain: 'blogplatform-backend.onrender.com',
+    // domain: 'blogplatform-backend.onrender.com',
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
